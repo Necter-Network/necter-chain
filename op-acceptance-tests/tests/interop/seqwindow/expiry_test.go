d3bdc7416a53155af24421727f4c7d56a45de15c
protolambda <proto@protolambda.com>
2025-05-20 22:26:51 +0200

    op-supervisor: resetTracker fixes (#16028)
    * op-supervisor: resetTracker draft changes
    
    Co-authored-by: Sebastian Stammler <seb@oplabs.co>
    
    * op-supervisor: syncnode reset tracker fixes
    
    * op-acceptance-tests: unlock sequencing window expiry test
    
    * op-supervisor,op-node,tests: fix sequencing window expiry test
    
    * op-supervisor,tests: fix review / ci
    
    ---------
    
    Co-authored-by: Sebastian Stammler <seb@oplabs.co>

--- op-acceptance-tests/tests/interop/seqwindow/expiry_test.go
+++ op-acceptance-tests/tests/interop/seqwindow/expiry_test.go
@@ -1,6 +1,7 @@
 package seqwindow
 
 import (
+	"strings"
 	"testing"
 	"time"
 
@@ -9,14 +10,14 @@ import (
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
 	"github.com/ethereum-optimism/optimism/op-devstack/presets"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum-optimism/optimism/op-service/retry"
 )
 
 // TestSequencingWindowExpiry tests that the sequencing window may expire,
 // the chain reorgs because of it, and that the chain then recovers.
+// This test can take 3 minutes to run.
 func TestSequencingWindowExpiry(gt *testing.T) {
 	t := devtest.SerialT(gt)
-	// TODO(#15769): fix sequencing-window expiry interop functionality
-	t.Skip("Interop sequencing-window expiry interaction is known to not fully work yet.")
 
 	sys := presets.NewSimpleInterop(t)
 	require := t.Require()
@@ -39,7 +40,14 @@ func TestSequencingWindowExpiry(gt *testing.T) {
 	t.Logger().Info("Tx 1 is safe now")
 
 	// Stop the batcher of chain A, so the L2 unsafe blocks will not get submitted.
-	require.NoError(sys.L2BatcherA.ActivityAPI().StopBatcher(t.Ctx()))
+	// This may take a while, since the batcher is still actively submitting new data.
+	require.NoError(retry.Do0(t.Ctx(), 10, retry.Exponential(), func() error {
+		err := sys.L2BatcherA.ActivityAPI().StopBatcher(t.Ctx())
+		if err != nil && strings.Contains(err.Error(), "batcher is not running") {
+			return nil
+		}
+		return err
+	}))
 	stoppedAt := sys.L1Network.WaitForBlock() // wait for new block, in case there is any batch left
 	// Make sure the supervisor has synced enough of the L1, for the local-safe query to work.
 	sys.Supervisor.AwaitMinL1(stoppedAt.Number)
@@ -75,12 +83,33 @@ func TestSequencingWindowExpiry(gt *testing.T) {
 		"tx", receipt2.TxHash, "l2Block", old)
 	// The logs will show a "Chain reorg detected" from op-geth.
 
+	// Once this happens we don't want to continue to try and include the tx in a block again,
+	// since that will then be reorged out again.
+	// We need to enter recovery-mode to not continue to build an incompatible chain that will not get submitted.
+	// It may reorg once more, but then stays compatible.
+	// For a while we'll have to build blocks that are not going to be reorged out due to subtle L1 origin divergence.
+	t.Logger().Info("Turning on recovery-mode")
+	t.Require().NoError(sys.L2CLA.Escape().RollupAPI().SetRecoverMode(t.Ctx(), true))
+
 	t.Logger().Info("Waiting for sequencing window expiry induced reorg now")
 	// Monitor that the old unsafe chain is reorged out as expected
 	require.Eventually(func() bool {
 		latest := sys.L2ELA.BlockRefByNumber(old.Number)
 		return latest.Hash != old.Hash
 	}, windowDuration+time.Second*20, time.Second, "expecting old block to be reorged out")
+
+	// Wait for the tx to no longer be included.
+	// The tx-indexer may still return the old block or be stale.
+	// So instead, lookup the tx nonce
+	require.Eventually(func() bool {
+		latestNonce, err := sys.L2ELA.Escape().EthClient().NonceAt(t.Ctx(), alice.Address(), nil)
+		if err != nil {
+			t.Logger().Error("Failed to look up pending nonce")
+			return false
+		}
+		t.Logger().Info("Checking tx 2 nonce", "latest", latestNonce, "tx2", tx2.Nonce.Value())
+		return latestNonce <= tx2.Nonce.Value()
+	}, windowDuration+time.Second*20, time.Second, "tx should be reorged out and not come back")
 
 	t.Logger().Info("Waiting for supervisor to surpass pre-reorg chain now")
 	// Monitor that the supervisor can continue to sync.
@@ -102,6 +131,39 @@ func TestSequencingWindowExpiry(gt *testing.T) {
 	other := sys.L2ELA.BlockRefByNumber(safe.Derived.Number)
 	require.Equal(safe.Derived.Hash, other.Hash, "supervisor must match chain with EL")
 
+	t.Logger().Info("Re-enabling batch-submitter")
 	// re-enable the batcher now that we are done with the test.
 	t.Require().NoError(sys.L2BatcherA.ActivityAPI().StartBatcher(t.Ctx()))
+	// TODO(#16036): batcher submits future span batch, misses a L2 block.
+	// For now it uses singular batches to work-around.
+
+	// Build the missing blocks, catch up on local-safe chain
+	t.Require().Eventually(func() bool {
+		status := sys.L2CLA.SyncStatus()
+		return status.LocalSafeL2.Number+10 > status.UnsafeL2.Number
+	}, windowDuration, time.Second*2, "wait for local-safe to recover near unsafe head")
+
+	// Once we have enough margin to not get reorged again before the batch-submitter acts,
+	// exit recovery mode, so we can include txs again.
+	t.Logger().Info("Exiting recovery mode")
+	t.Require().NoError(sys.L2CLA.Escape().RollupAPI().SetRecoverMode(t.Ctx(), false))
+
+	// Now confirm a tx, chain should be healthy again.
+	tx3 := alice.Transfer(common.HexToAddress("0x7777"), eth.GWei(100))
+	receipt3, err := tx3.Included.Eval(t.Ctx())
+	require.NoError(err)
+	t.Logger().Info("Confirmed tx 3", "tx", receipt3.TxHash, "block", receipt3.BlockHash)
+
+	// Wait for the first tx to become cross-safe.
+	// We are not interested in the sequencing window to expire and revert all the way back to 0.
+	require.Eventually(func() bool {
+		status := sys.L2CLA.SyncStatus()
+		t.Logger().Info("Awaiting tx safety",
+			"local-unsafe", status.UnsafeL2, "local-safe", status.LocalSafeL2)
+		return status.SafeL2.Number > receipt3.BlockNumber.Uint64()
+	}, time.Second*60, time.Second, "wait for tx 3 to be safe")
+	t.Logger().Info("Tx 3 is safe now")
+	// Sanity check the block the tx was included is really still canonical
+	got := sys.L2ELA.BlockRefByNumber(receipt3.BlockNumber.Uint64())
+	t.Require().Equal(receipt3.BlockHash, got.Hash, "tx 3 was included in canonical block")
 }
